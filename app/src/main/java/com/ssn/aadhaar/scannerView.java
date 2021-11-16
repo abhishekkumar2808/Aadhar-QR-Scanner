5eb16a6df0b1901241176b970197de5e62dbd4c2
abhishekkumar2808 <abhishekk2808@gmail.com>
2021-11-17 01:45:27 +0530

    JSON conversion done

--- app/src/main/java/com/ssn/aadhaar/scannerView.java
+++ app/src/main/java/com/ssn/aadhaar/scannerView.java
@@ -4,6 +4,7 @@ import androidx.appcompat.app.AppCompatActivity;
 
 import android.Manifest;
 import android.os.Bundle;
+import android.sax.Element;
 
 import com.google.zxing.Result;
 import com.karumi.dexter.Dexter;
@@ -12,6 +13,19 @@ import com.karumi.dexter.listener.PermissionDeniedResponse;
 import com.karumi.dexter.listener.PermissionGrantedResponse;
 import com.karumi.dexter.listener.PermissionRequest;
 import com.karumi.dexter.listener.single.PermissionListener;
+
+import org.json.JSONException;
+import org.json.JSONObject;
+import org.w3c.dom.Document;
+import org.w3c.dom.NodeList;
+import org.xml.sax.InputSource;
+import org.xml.sax.SAXException;
+
+import java.io.IOException;
+import java.io.StringReader;
+
+import javax.xml.parsers.DocumentBuilderFactory;
+import javax.xml.parsers.ParserConfigurationException;
 
 import me.dm7.barcodescanner.zxing.ZXingScannerView;
 
@@ -46,9 +60,43 @@ public class scannerView extends AppCompatActivity implements ZXingScannerView.R
     }
 
     @Override
-    public void handleResult(Result rawResult) {
-       MainActivity.scantext.setText(rawResult.getText());
-       onBackPressed();
+    public void handleResult(Result rawResult)
+    {
+
+       String data = rawResult.getText();
+       System.out.println(data);
+
+        try {
+            toJSON(data);
+        } catch (JSONException e) {
+            e.printStackTrace();
+        }
+
+        onBackPressed();
+    }
+
+    private void toJSON(String data) throws JSONException {
+
+        String[] listOfString = data.split("\n",3);
+
+        String metaData = listOfString[1];
+        metaData = metaData.substring(24,metaData.length()-2);
+        String[] actualList = metaData.split("\" ");
+
+        JSONObject json = new JSONObject();
+        for(String i:actualList)
+        {
+            i = i.replaceAll("\"","");
+            //System.out.println("\n"+i);
+            int equalTO = i.indexOf("=");
+            String beforeEqual = i.substring(0,equalTO);
+            String afterEqual = i.substring(equalTO+1);
+            json.put(beforeEqual,afterEqual);
+
+        }
+        System.out.println("\n\nJSON data = "+json.toString());
+        MainActivity.scantext.setText(json.toString());
+
     }
 
     @Override
