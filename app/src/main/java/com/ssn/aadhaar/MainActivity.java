e82463f26f7f25f33716dc52ed24ea5ffc6fc662
abhishekkumar2808 <abhishekk2808@gmail.com>
2021-11-16 15:43:26 +0530

    qr scanner done

---
+++ app/src/main/java/com/ssn/aadhaar/MainActivity.java
@@ -0,0 +1,32 @@
+package com.ssn.aadhaar;
+
+import androidx.appcompat.app.AppCompatActivity;
+
+import android.content.Intent;
+import android.os.Bundle;
+import android.view.View;
+import android.widget.Button;
+import android.widget.TextView;
+
+import com.kazimasum.qrdemo.R;
+
+public class MainActivity extends AppCompatActivity {
+
+    Button scanbtn;
+    public static TextView scantext;
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_main);
+
+        scantext=(TextView)findViewById(R.id.scantext);
+        scanbtn=(Button) findViewById(R.id.scanbtn);
+
+            scanbtn.setOnClickListener(new View.OnClickListener() {
+                @Override
+                public void onClick(View view) {
+                    startActivity(new Intent(getApplicationContext(),scannerView.class));
+                }
+            });
+    }
+}
